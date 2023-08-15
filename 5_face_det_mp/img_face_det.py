a4a7e09b42519fed1651c71e2ce35c041aa9b7ba
fudiwara <tfuji@do-johodai.ac.jp>
2023-08-16 01:47:38 +0900

    add 5

---
+++ 5_face_det_mp/img_face_det.py
@@ -0,0 +1,32 @@
+import sys
+import pathlib
+import cv2
+import mediapipe as mp
+
+mp_face_detection = mp.solutions.face_detection # mediapipeの初期化
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence = 0.5)
+
+img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR) # 画像読み込み
+ch, cw, _ = img.shape # 画像サイズ取得
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # グレースケール変換
+faces = face_det.detectMultiScale(img_gray) # 顔検出
+
+results = face_detection.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # mediapipeに処理を渡す
+if results.detections:
+    for i in range(len(results.detections)):
+        # 顔領域の検出結果描画
+        b = results.detections[i].location_data.relative_bounding_box
+        x0 = int(b.xmin * cw)
+        y0 = int(b.ymin * ch)
+        x1 = int((b.xmin + b.width) * cw)
+        y1 = int((b.ymin + b.height) * ch)
+        cv2.rectangle(img, (x0, y0), (x1, y1), (0, 255, 0))
+
+# 読み込んだファイル名に文字列を追加したものを用意する
+input_file_name = pathlib.Path(sys.argv[1])
+output_filename = input_file_name.stem + "_fdetmp" + input_file_name.suffix
+output_file_path = input_file_name.parent / output_filename
+
+print(output_file_path) # 標準出力に出してPHPでひろう
+cv2.imwrite(str(output_file_path), img) # ファイル保存
---
+++ 5_face_det_mp/manage_image.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+
+if( !empty($_POST["proc_fdet"]) ){ // 「顔検出」ボタンを押してリロードする時の処理：顔検出本体
+	$com = "python3 img_face_det.py ". $_SESSION["proc_target_file_name"];
+	exec($com, $dst_filename, $ret); // execでpythonの処理を実行
+	$_SESSION["dst_filename"] = $dst_filename[1]; // 実行結果で標準出力されたファイル名を得る (mp用)
+	$_SESSION["flag_dst"] = true;
+
+}else{ // 最初に開く時点での処理：画像のアップロード
+	$tmp_file_name = $_FILES["upfile"]["tmp_name"]; // POSTで得た仮のファイル名
+	$src_file_name = $_FILES["upfile"]["name"]; // 元のファイル名
+
+	$up_file_name  = ""; // サーバ上で保存するファイル名
+	$flag_upload = false; // アップロードが完了しているかのフラグ
+	if( is_uploaded_file($tmp_file_name) ){ // アップロード成功の可否 (やりようによっては必要なし)
+		$split_str = explode('.', $src_file_name);
+		$ext = end($split_str); // ↑でピリオドで区切った最後のブロックを拡張子とする
+		if($ext != "" && $ext != $src_file_name){ // 拡張子が得られたら
+			$up_file_name = "image/". date("Ymd_His") . ".$ext";
+			$flag_upload = move_uploaded_file($tmp_file_name, $up_file_name); // 仮置き場から移動
+			$_SESSION["flag_upload"] = $flag_upload;
+			$_SESSION["up_file_name"] = $up_file_name;
+			$_SESSION["proc_target_file_name"] = $up_file_name;
+		}
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>image image</title>
+</head>
+<body>
+<?php
+if( $_SESSION["flag_upload"] ){ // ファイルアップロードが完了している場合
+?>
+	<p>アップロードされた画像 ↓↓<br>
+	<img src="<?= $_SESSION["up_file_name"] ?>"></p>
+
+	<form method="post">
+		<p><input type="submit" name="proc_fdet" value="顔検出"></p>
+	</form>
+<?php
+}
+if( !empty($_SESSION["flag_dst"]) ){ // ファイルアップロードが完了している場合
+?>
+	<p>顔検出の結果 ↓↓<br>
+	<img src="<?= $_SESSION["dst_filename"] ?>"></p>
+
+<?php
+}
+if( !$_SESSION["flag_upload"] ){ // ファイルアップロードが完了している場合
+?>
+	<p>アップロードは失敗しました。</p>
+<?php
+}
+?>
+	<a href="index.php">アップロードフォームへ戻る</a>
+</body>
+</html>
